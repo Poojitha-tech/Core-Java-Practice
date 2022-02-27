2643b4a86b68e121b1045a37aa29c7eecab716e5
Poojitha-tech <73094494+Poojitha-tech@users.noreply.github.com>
2022-02-27 13:03:53 +0530

    Add files via upload

---
+++ student.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Example1 {
+public static void main(String[] args) { Scanner s=new Scanner(System.in); System.out.println("Enter Project marks:");
+int PM=s.nextInt();
+System.ouy.println("Enter internal marks: ");
+int IM=s.nextInt();
+system.out.println("Enter External marks: ");
+int EM=s.nextInt();
+if((PM>=50 && PM<=100) && (IM>=50 && IM<=100) && (EM>=50 && EM<=100)){
+int total_score=(((PM*70)/100)+((IM*20)/100)+((EM*10)/100))
+}
+}
+}
+
+
